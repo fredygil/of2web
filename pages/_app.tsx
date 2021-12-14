5686857436e7e885012385ad07a79c0017c2350e
cfredygilmejia <fgil666@gmail.com>
2021-12-13 22:01:31 -0500

    add ant design library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { StoreProvider } from "easy-peasy";
 import { store } from "../client/store";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
